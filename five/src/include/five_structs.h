4246314a7b50b9bd81ffcae61d48259f3bca222f
mngan <mei@nist.gov>
2024-01-23 13:04:03 -0500

    Pushing header files for FIVE 2024

---
+++ five/src/include/five_structs.h
@@ -0,0 +1,370 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology (NIST) by employees of the Federal Government in the course
+ * of their official duties. Pursuant to title 17 Section 105 of the
+ * United States Code, this software is not subject to copyright protection
+ * and is in the public domain. NIST assumes no responsibility whatsoever for
+ * its use by other parties, and makes no guarantees, expressed or implied,
+ * about its quality, reliability, or any other characteristic.
+ */
+
+#ifndef FIVE_STRUCTS_H_
+#define FIVE_STRUCTS_H_
+
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace FIVE {
+/**
+ * @brief
+ * Struct representing a single image
+ */
+typedef struct Image {
+    /** Labels describing the type of image */
+
+    // ok so we have still/video below so remove the "Still"/"Video" from enum below
+
+    enum class ImageDescription {
+        /** Image/frame with unknown or unassigned collection conditions.
+         *  Media labeled as "Unknown" could include any category of imagery, 
+         *  and developers are expected to handle this appropriately.
+         */
+        Unknown = 0,
+        /** Face image, Frontal closely ISO/IEC 19794-5:2005 compliant */
+        StillISO = 1,
+        /** Face image from law enforcement booking processes, nominally frontal */
+        StillMugshot = 2,
+        /** Face image that might appear in a news source or magazine. The images are typically
+         * typically well exposed and focused but exhibit pose and illumination variations. */
+        StillPhotojournalism = 3,
+        /** Unconstrained face, taken by amateur photographer, widely varying pose, illumination, resolution */
+        StillWild = 4,
+        /** Frame from video collected at long range (and potentially under turbulent conditions) */
+        VideoLongRange = 5,
+        /** Frame from video from television footage */ 
+        VideoPhotojournalism = 6,
+        /** Frame from video collected passively in spaces such as sports arenas, airports, etc.) */
+        VideoPassiveObservation = 7,
+        /** Frame from video collected at a chokepoint */
+        VideoChokepoint = 8,
+        /** Frame from video collected from elevated platforms for which look-down angle is large */
+        VideoElevatedPlatform = 9
+    };  
+       
+    enum class Illuminant {
+        /** Not specified */
+        Unspecified = 0,
+        /** Conventional visible light */
+        Visible = 1,
+    };
+
+    /** Number of pixels horizontally */
+    uint16_t width;
+    /** Number of pixels vertically */
+    uint16_t height;
+    /** Number of bits per pixel. Legal values are 8 and 24. */
+    uint8_t depth;
+    /** Managed pointer to raster scanned data.
+     * Either RGB color or intensity.
+     * If image_depth == 24 this points to  3WH bytes  RGBRGBRGB...
+     * If image_depth ==  8 this points to  WH bytes  IIIIIII */
+    std::shared_ptr<uint8_t> data;
+    /** Single description of the image.  */     
+    ImageDescription description;
+    /** Source of light used to acquire the image */
+    Illuminant illuminant;
+
+    Image() :
+        width{0},
+        height{0},
+        depth{24},
+        description{ImageDescription::Unknown},
+        illuminant{Illuminant::Visible}
+        {}
+
+    Image(
+        uint16_t width,
+        uint16_t height,
+        uint8_t depth,
+        std::shared_ptr<uint8_t> &data,
+        ImageDescription description,
+        Illuminant illuminant
+        ) :
+        width{width},
+        height{height},
+        depth{depth},
+        data{data},
+        description{description},
+        illuminant{illuminant}
+        {}
+
+    /** @brief This function returns the size of the image data. */
+    size_t
+    size() const { return (width * height * (depth / 8)); }
+} Image;
+
+/**
+ * @brief
+ * Struct representing a piece of media
+ */
+typedef struct Media {
+    /** Labels describing the type of media */
+    enum class Label {
+        /** Still photos of an individual */
+        Image = 0,
+        /** Sequential video frames of an individual */
+        Video = 1
+    };
+
+    /** Type of media */
+    Label type;
+    /** Vector of still image(s) or video frames */
+    std::vector<FIVE::Image> data;
+    /** For video data, the frame rate in frames per second */
+    uint8_t fps;
+
+    Media() :
+        type{Media::Label::Image},
+        fps{0}
+        {}
+
+    Media(
+        const Media::Label type,
+        const std::vector<FIVE::Image> &data,
+        const uint8_t fps
+        ) :
+        type{type},
+        data{data},
+        fps{fps}
+        {}
+} Media;
+
+/** Labels describing the composition of the 1:N gallery
+ *  (provided as input into gallery finalization function)
+ */
+enum class GalleryType {
+    /** Consolidated, subject-based */
+    Consolidated = 0,
+    /** Unconsolidated, event-based */
+    Unconsolidated = 1
+};
+
+/**
+ * @brief
+ * Data structure for result of an identification search
+ */
+typedef struct Candidate {
+    /** @brief If the candidate is valid, this should be set to true. If
+     * the candidate computation failed, this should be set to false.
+     * If value is set to false, score and templateId
+     * will be ignored entirely. */
+    bool isAssigned;
+
+    /** @brief The template ID from the enrollment database manifest */
+    std::string templateId;
+
+    /** @brief Measure of similarity or dissimilarity between the identification template
+     * and the enrolled candidate.
+     * For face recognition, a similarity score - higher is more similar;
+     * For iris recognition, a non-negative measure of dissimilarity (maybe a distance) - lower is more similar;
+     * For multimodal face and iris, a similarity score - higher is more similar;
+     */
+    double score;
+
+    Candidate() :
+        isAssigned{false},
+        templateId{""},
+        score{-1.0}
+        {}
+
+    Candidate(
+        bool isAssigned,
+        std::string templateId,
+        double score) :
+        isAssigned{isAssigned},
+        templateId{templateId},
+        score{score}
+        {}
+} Candidate;
+
+/**
+ * @brief
+ * Return codes for functions specified in this API
+ */
+enum class ReturnCode {
+    /** Success */
+    Success = 0,
+    /** Catch-all error */
+    UnknownError = 1,
+    /** Error reading configuration files */
+    ConfigError = 2,
+    /** Elective refusal to process the input */
+    RefuseInput = 3,
+    /** Involuntary failure to process the image */
+    ExtractError = 4,
+    /** Cannot parse the input data */
+    ParseError = 5,
+    /** Elective refusal to produce a template */
+    TemplateCreationError = 6,
+    /** Either or both of the input templates were result of failed feature extraction */
+    VerifTemplateError = 7,
+    /** Unable to detect a face in the image */
+    FaceDetectionError = 8,
+    /** The implementation cannot support the number of input images */
+    NumDataError = 9,
+    /** Template file is an incorrect format or defective */
+    TemplateFormatError = 10,
+    /** An operation on the enrollment directory failed (e.g. permission, space) */
+    EnrollDirError = 11,
+    /** Cannot locate the input data - the input files or names seem incorrect */
+    InputLocationError = 12,
+    /** Memory allocation failed (e.g. out of memory) */
+    MemoryError = 13,
+    /** Function is not implemented */
+    NotImplemented = 14,
+    /** Vendor-defined failure */
+    VendorError = 15
+};
+
+/** Output stream operator for a ReturnCode object. */
+inline std::ostream&
+operator<<(
+    std::ostream &s,
+    const ReturnCode &rc)
+{
+    switch (rc) {
+    case ReturnCode::Success:
+        return (s << "Success");
+    case ReturnCode::UnknownError:
+        return (s << "Unknown Error");
+    case ReturnCode::ConfigError:
+        return (s << "Error reading configuration files");
+    case ReturnCode::RefuseInput:
+        return (s << "Elective refusal to process the input");
+    case ReturnCode::ExtractError:
+        return (s << "Involuntary failure to process the image");
+    case ReturnCode::ParseError:
+        return (s << "Cannot parse the input data");
+    case ReturnCode::TemplateCreationError:
+        return (s << "Elective refusal to produce a template");
+    case ReturnCode::VerifTemplateError:
+        return (s << "Either or both of the input templates were result of "
+                "failed feature extraction");
+    case ReturnCode::FaceDetectionError:
+        return (s << "Unable to detect a face in the image");
+    case ReturnCode::NumDataError:
+        return (s << "Number of input images not supported");
+    case ReturnCode::TemplateFormatError:
+        return (s << "Template file is an incorrect format or defective");
+    case ReturnCode::EnrollDirError:
+        return (s << "An operation on the enrollment directory failed");
+    case ReturnCode::InputLocationError:
+        return (s << "Cannot locate the input data - the input files or names "
+                "seem incorrect");
+    case ReturnCode::MemoryError:
+        return (s << "Memory allocation failed (e.g. out of memory)");
+    case ReturnCode::NotImplemented:
+        return (s << "Function is not implemented");
+    case ReturnCode::VendorError:
+        return (s << "Vendor-defined error");
+    default:
+        return (s << "Undefined error");
+    }
+}
+
+/**
+ * @brief
+ * A structure to contain information about a failure by the software
+ * under test.
+ *
+ * @details
+ * An object of this class allows the software to return some information
+ * from a function call. The string within this object can be optionally
+ * set to provide more information for debugging etc. The status code
+ * will be set by the function to Success on success, or one of the
+ * other codes on failure.
+ */
+typedef struct ReturnStatus {
+    /** @brief Return status code */
+    ReturnCode code;
+    /** @brief Optional information string */
+    std::string info;
+
+    ReturnStatus() :
+        code{ReturnCode::UnknownError},
+        info{""}
+        {}
+    /**
+     * @brief
+     * Create a ReturnStatus object.
+     *
+     * @param[in] code
+     * The return status code; required.
+     * @param[in] info
+     * The optional information string.
+     */
+    ReturnStatus(
+        const ReturnCode code,
+        const std::string &info = ""
+        ) :
+        code{code},
+        info{info}
+        {}
+} ReturnStatus;
+
+typedef struct BoundingBox
+{
+    /** @brief leftmost point on head, typically subject's right ear
+     *  value must be on [0, imageWidth-1] */
+    int16_t xleft;
+    /** @brief high point of head, typically top of hair
+     *  value must be on [0, imageHeight-1] */
+    int16_t ytop;
+    /** @brief bounding box width */
+    int16_t width;
+    /** @brief bounding box height */
+    int16_t height;
+
+    BoundingBox() :
+        xleft{-1},
+        ytop{-1},
+        width{-1},
+        height{-1}
+        {}
+
+    BoundingBox(
+        int16_t xleft,
+        int16_t ytop,
+        int16_t width,
+        int16_t height) :
+        xleft{xleft},
+        ytop{ytop},
+        width{width},
+        height{height}
+        {}
+} BoundingBox;
+
+/*
+* Versioning
+*
+* NIST code will extern the version number symbols. Participant
+* shall compile them into their core library.
+*/
+#ifdef NIST_EXTERN_FIVE_STRUCTS_VERSION
+/** major version number. */
+extern uint16_t FIVE_STRUCTS_MAJOR_VERSION;
+/** minor version number. */
+extern uint16_t FIVE_STRUCTS_MINOR_VERSION;
+#else /* NIST_EXTERN_FIVE_STRUCTS_VERSION */
+/** major version number. */
+uint16_t FIVE_STRUCTS_MAJOR_VERSION{0};
+/** minor version number. */
+uint16_t FIVE_STRUCTS_MINOR_VERSION{1};
+#endif /* NIST_EXTERN_FIVE_STRUCTS_VERSION */
+}
+
+#endif /* FIVE_STRUCTS_H_ */
---
+++ five/src/include/frte_five.h
@@ -0,0 +1,279 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology (NIST) by employees of the Federal Government in the course
+ * of their official duties. Pursuant to title 17 Section 105 of the
+ * United States Code, this software is not subject to copyright protection
+ * and is in the public domain. NIST assumes no responsibility whatsoever for
+ * its use by other parties, and makes no guarantees, expressed or implied,
+ * about its quality, reliability, or any other characteristic.
+ */
+
+#ifndef FRTE_FIVE_H_
+#define FRTE_FIVE_H_
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include <five_structs.h>
+
+namespace FIVE {
+
+/**
+ * @brief
+ * The interface to the FIVE implementation
+ *
+ * @details
+ * The submission software under test will implement this interface by
+ * sub-classing this class and implementing each method therein.
+ */
+class Interface {
+public:
+    virtual ~Interface() {}
+
+    /**
+     * @brief Before imagery is sent to the template
+     * creation function(s), the test harness will call this initialization
+     * function.
+     * @details This function will be called N=1 times by the NIST application,
+     * prior to parallelizing M >= 1 calls to createTemplate() via fork().
+     *
+     * This function will be called from a single process/thread.
+     *
+     * @param[in] configDir
+     * A read-only directory containing any developer-supplied configuration
+     * parameters or run-time data files.
+     */
+    virtual FIVE::ReturnStatus
+    initializeTemplateCreation(
+        const std::string &configDir) = 0;
+
+    /**
+     * @brief This function supports enrollment template generation from one or more pieces
+     * of media of the same person.  It takes a vector of input media and outputs a single 
+     * template.
+     *
+     * @details
+     * - The input media vector can contain any combination of one or more 
+     *   videos or stills for a single subject.
+     * - Media only contains a single person
+     *
+     * If the function executes correctly (i.e. returns a successful exit status),
+     * the template(s) will be enrolled into a gallery.  The NIST
+     * calling application may store the resulting template(s),
+     * concatenate many templates, and pass the result to the enrollment
+     * finalization function.  When the implementation fails to produce a
+     * template, it shall still return a blank template (which can be zero
+     * bytes in length). The template will be included in the
+     * enrollment database/manifest like all other enrollment templates, but
+     * is not expected to contain any feature information.
+     * 
+     * param[in] media
+     * A vector of Media objects
+     * param[out] personTemplate
+     * A single output template.  The format of the template is entirely
+     * unregulated.  
+     * param[out] personTracks 
+     * For each still/frame in each piece of media, the function shall return
+     * bounding boxes around the detected face for the subject. 
+     * This will be an empty vector when passed into the
+     * function, and the implementation shall populate it with the appropriate
+     * number of entries.  Values in personTracks[i] shall correspond to input media[i].
+     */
+    virtual FIVE::ReturnStatus
+    createEnrollmentTemplate(
+        const std::vector<FIVE::Media> &media,
+        std::vector<uint8_t> &personTemplate,
+        std::vector< std::vector<FIVE::BoundingBox> > &personTracks) = 0;
+
+    /**
+     * @brief This function supports search/probe/identification template generation of one or more people detected
+     * in the provided input media.  It takes a single piece of input media (still photo or video frames) 
+     * and outputs one or more templates and tracks based on the number of people detected.
+     *
+     * @details
+     * If the function returns a non-successful return status, the output template(s) will be not be used
+     * in subsequent search operations.
+     *
+     * param[in] media
+     * A single piece of media (still photo or video frames) that contains one or more people in the photo
+     * param[out] personTemplates 
+     * A vector of output template(s).  The format of the template(s) is entirely
+     * unregulated.  This will be an empty vector when passed into the function, and
+     * the implementation can resize and populate it with the appropriate data.
+     * param[out] personTracks
+     * For each person detected in the media, the function shall return
+     * bounding boxes around the detected face.
+     * This will be an empty vector when passed into the
+     * function, and the implementation shall populate it with the appropriate
+     * number of entries.  Values in personTracks[i] shall correspond to the 
+     * person in personTemplates[i].  
+     */
+    virtual FIVE::ReturnStatus
+    createSearchTemplate(
+        const FIVE::Media &media,
+        std::vector< std::vector<uint8_t> > &personTemplates,
+        std::vector< std::vector<FIVE::BoundingBox> > &personTracks) = 0;
+
+     /**
+      * @brief This function will be called after all enrollment templates have
+      * been created and freezes the enrollment data.
+      * After this call, the enrollment dataset will be forever read-only.
+      *
+      * @details This function allows the implementation to conduct,
+      * for example, statistical processing of the feature data, indexing and
+      * data re-organization.  The function may create its own data structure.
+      * It may increase or decrease the size of the stored data.  No output is
+      * expected from this function, except a return code.  The function will
+      * generally be called in a separate process after all the enrollment processes
+      * are complete.
+      * NOTE: Implementations shall not move the input data.  Implementations
+      * shall not point to the input data.
+      * Implementations should not assume the input data would be readable
+      * after the call.  Implementations must,
+      * <b>at a minimum, copy the input data</b> or otherwise extract what is
+      * needed for search.
+      *
+      * This function will be called from a single process/thread.
+      *
+      * @param[in] configDir
+      * A read-only directory containing any developer-supplied configuration
+      * parameters or run-time data files.
+      * @param[in] enrollmentDir
+      * The top-level directory in which enrollment data was placed. This
+      * variable allows an implementation
+      * to locate any private initialization data it elected to place in the
+      * directory.
+      * @param[in] edbName
+      * The name of a single file containing concatenated templates, i.e. the
+      * EDB described in <em>File structure for enrolled template collection</em>.
+      * While the file will have read-write-delete permission, the implementation
+      * should only alter the file if it preserves the necessary content, in
+      * other files for example.
+      * The file may be opened directly.  It is not necessary to prepend a
+      * directory name.  This is a NIST-provided
+      * input - implementers shall not internally hard-code or assume any values.
+      * @param[in] edbManifestName
+      * The name of a single file containing the EDB manifest described in
+      * <em>File structure for enrolled template collection</em>.
+      * The file may be opened directly.  It is not necessary to prepend a
+      * directory name.  This is a NIST-provided
+      * input - implementers shall not internally hard-code or assume any values.
+      * @param[in] galleryType
+      * The composition of the gallery as enumerated by GalleryType.
+      */
+    virtual FIVE::ReturnStatus
+    finalizeEnrollment(
+    	const std::string &configDir,
+        const std::string &enrollmentDir,
+        const std::string &edbName,
+        const std::string &edbManifestName,
+        FIVE::GalleryType galleryType) = 0;
+
+    /**
+     * @brief This function will be called once prior to one or more calls to
+     * search().  The function might set static internal variables
+     * and read the enrollment gallery into memory
+     * so that the enrollment database is available to the subsequent
+     * identification searches.
+     *
+     * This function will be called from a single process/thread.
+     *
+     * @param[in] configDir
+     * A read-only directory containing any developer-supplied configuration
+     * parameters or run-time data files.
+     * @param[in] enrollmentDir
+     * The read-only top-level directory in which enrollment data was placed.
+     */
+    virtual FIVE::ReturnStatus
+    initializeSearch(
+        const std::string &configDir,
+        const std::string &enrollmentDir) = 0;
+
+    /**
+     * @brief This function searches a probe/identification template against the
+     * enrollment set, and outputs a vector containing candidateListLength
+     * Candidates.
+     *
+     * @details Each candidate shall be populated by the implementation
+     * and added to candidateList.  Note that candidateList will be an empty
+     * vector when passed into this function.  The candidates shall appear in descending order
+     * of similarity score - i.e., the most similar entries appear first.
+     *
+     * @param[in] idTemplate
+     * A template from the implemented search template creation function.  If the value 
+     * returned by that function was non-successful, the contents of idTemplate will not be
+     * used, and this function will not be called.
+     *
+     * @param[in] candidateListLength
+     * The number of candidates the search should return.
+     * @param[out] candidateList
+     * Each candidate shall be populated by the implementation.  The candidates
+     * shall appear in descending order of similarity score - i.e., the most similar
+     * entries appear first.
+     */
+    virtual FIVE::ReturnStatus
+    search(
+        const std::vector<uint8_t> &searchTemplate,
+        const uint32_t &candidateListLength,
+        std::vector<FIVE::Candidate> &candidateList) = 0;
+
+    /**
+     * @brief This function outputs the threshold that corresponds to an intended/target false positive
+     * identification rate (FPIR) 
+     *
+     * @details This function returns what the developer thinks the threshold should be set 
+     * to (not what NIST generates) for a target FPIR.  This function would be useful to end-users 
+     * for system configuration.  The goal is to foster a generic threshold regardless of type of 
+     * imagery, demographics, etc. that yields predictable FPIR. 
+     *
+     * @param[in] numPeopleInGallery
+     * The number of people enrolled in a gallery/database
+     *
+     * @param[in] intendedFPIR
+     * The target FPIR of interest
+     *
+     * @param[out] threshold
+     * The threshold that corresponds to the intended FPIR
+     */ 
+    virtual FIVE::ReturnStatus
+    getThreshold(
+        const uint32_t &numPeopleInGallery,
+        const double &intendedFPIR,
+        double &threshold) = 0; 
+
+    /**
+     * @brief
+     * Factory method to return a managed pointer to the Interface object.
+     * @details
+     * This function is implemented by the submitted library and must return
+     * a managed pointer to the Interface object.
+     *
+     * @note
+     * A possible implementation might be:
+     * return (std::make_shared<Implementation>());
+     */
+    static std::shared_ptr<Interface>
+    getImplementation();
+};
+
+/*
+ * API versioning
+ *
+ * NIST code will extern the version number symbols.
+ * Participant shall compile them into their core library.
+ */
+#ifdef NIST_EXTERN_API_VERSION
+/** API major version number. */
+extern uint16_t API_MAJOR_VERSION;
+/** API minor version number. */
+extern uint16_t API_MINOR_VERSION;
+#else /* NIST_EXTERN_API_VERSION */
+/** API major version number. */
+uint16_t API_MAJOR_VERSION{0};
+/** API minor version number. */
+uint16_t API_MINOR_VERSION{1};
+#endif /* NIST_EXTERN_API_VERSION */
+}
+
+#endif /* FRTE_FIVE_H_ */
